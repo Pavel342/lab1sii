364612e0affffbc08fd1da0d4fd47085ff3f0833
Pavel <pavel.samoylo.02@mail.ru>
2022-02-22 18:24:45 +0300

    so so

--- src/programm/one/formula.java
+++ src/programm/one/formula.java
@@ -1,16 +1,17 @@
 package programm.one;
 
-public class Main {
+public class formula {
     public static void main(String[] args) {
         float net = 0;
         int[] y = new int[16];
-        float nn = 0.7F;//норма обучения
+        float f;
+        float nn = 0.5F;//норма обучения
         int k = 0;//эпоха
         float[] w = new float[]{0, 0, 0, 0, 0};
         int[][] x = {{1, 0, 0, 0, 0}, {1, 0, 0, 0, 1}, {1, 0, 0, 1, 0}, {1, 0, 0, 1, 1}, {1, 0, 1, 0, 0}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 0}, {1, 0, 1, 1, 1}, {1, 1, 0, 0, 0}, {1, 1, 0, 0, 1}, {1, 1, 0, 1, 0}, {1, 1, 0, 1, 1}, {1, 1, 1, 0, 0}, {1, 1, 1, 0, 1}, {1, 1, 1, 1, 0}, {1, 1, 1, 1, 1}};
-       int[] otveti = new int[]{0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1};
+        int[] otveti = new int[]{0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1};
         int be;
-        int sumMis ;
+        int sumMis;
         boolean kaif = true;
         do {
             sumMis = 0;
@@ -23,14 +24,15 @@ public class Main {
                     net += w[j] * x[i][j];
                 }
                 net += w[0];
-                if (net >= 0) y[i] = 1;
+                f = (float) (1 / (1 + Math.E * (-1 * net)));
+                if (f >= 0.5) y[i] = 1;
                 else y[i] = 0;
                 be =  otveti[i]-y[i] ;
-                if (y[i] == otveti[i]) ;
+                if (y[i] == otveti[i])  ;
                 else {
                     sumMis++;
                     kaif = false;
-                    smena(w, be, nn, x, i);
+                    smena(w, be, nn, x, i, f);
                 }
                 if (!kaif) System.out.println("Не верно");
                 else System.out.println("Верно");
@@ -43,13 +45,10 @@ public class Main {
 
     }
 
-    public static float[] smena ( float[] w, int be, float nn, int[][] x, int l){
+    public static float[] smena ( float[] w, int be, float nn, int[][] x, int l, float f){
         for (int i = 0; i < 5; i++) {
-            w[i] = w[i] + nn * be * x[l][i];
+            w[i] = w[i]+ f*(1-f) * nn * be * x[l][i];
         }
         return w;
     }
-
-
-
 }
